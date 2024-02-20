501128b8c99eb87d7a02c7ac29e2fdaf0434df35
Gary L Bentley <45616105+GaryLBentley@users.noreply.github.com>
2024-02-19 19:03:07 -0800

    Determining which flights are delayed

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ WGU D502 BSDA Capstone Project
 
 US International Airports
 https://www.nationsonline.org/oneworld/major_US_airports.htm
+
+Time conversion functions
+https://www.kaggle.com/code/fabiendaniel/predicting-flight-delays-tutorial
+Available under Apache 2.0 license https://www.apache.org/licenses/LICENSE-2.0
--- time_functions.py
+++ time_functions.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import datetime
 
+# Time functions reused under Apache 2.0 License
+# https://www.kaggle.com/code/fabiendaniel/predicting-flight-delays-tutorial
+# Original author: FABIENDANIEL 
+# Some code has been refactored to account for deprecations in Pandas
+
 # Function that convert the 'HHMM' string to datetime.time
 def format_hour(date_string):
     if pd.isnull(date_string):
@@ -14,22 +19,22 @@ def format_hour(date_string):
 #_____________________________________________________________________
 # Function that combines a date and time to produce a datetime.datetime
 def date_hour(x):
-    if pd.isnull(x[0]) or pd.isnull(x[1]):
+    if pd.isnull(x.iloc[0]) or pd.isnull(x.iloc[1]):
         return np.nan
     else:
-        return datetime.datetime.combine(x[0],x[1])
+        return datetime.datetime.combine(x.iloc[0],x.iloc[1])
 #_______________________________________________________________________________
 # Function that combine two columns of the dataframe to create a datetime format
 def flight_time(df, col):    
     time_list = []
     for index, cols in df[['DATE', col]].iterrows():    
-        if pd.isnull(cols[1]):
+        if pd.isnull(cols.iloc[1]):
             time_list.append(np.nan)
-        elif float(cols[1]) == 2400:
-            cols[0] += datetime.timedelta(days=1)
-            cols[1] = datetime.time(0,0)
+        elif float(cols.iloc[1]) == 2400:
+            cols.iloc[0] += datetime.timedelta(days=1)
+            cols.iloc[1] = datetime.time(0,0)
             time_list.append(date_hour(cols))
         else:
-            cols[1] = format_hour(cols[1])
+            cols.iloc[1] = format_hour(cols.iloc[1])
             time_list.append(date_hour(cols))
     return pd.Series(time_list)
